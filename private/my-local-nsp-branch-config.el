e7038a7b63917de63b40655a9ee80f8c2799e422
johanst <johan.sternerup@gmail.com>
2014-03-17 16:10:11 +0100

    huh

--- private/my-local-nsp-branch-config.el
+++ private/my-local-nsp-branch-config.el
@@ -1,4 +1,4 @@
-(nsp-add-branch "DEV-SP115" "/home/johanst/DEV-SP115/server")
-(nsp-add-branch "INT-V160-PB" "/home/johanst/INT-V160-PB/server")
+(nsp-add-branch "INT-V150" "/home/johanst/extflash/INT-V150/server")
+(nsp-add-branch "INT-V151-DSN" "/home/johanst/INT-V151-DSN/server")
 
 (provide 'my-local-nsp-branch-config)

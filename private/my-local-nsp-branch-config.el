b208f4e82a9e5e4ffb2fe266754dd40a2106ee67
johanst <johan.sternerup@non.schneider-electric.com>
2014-07-01 16:13:39 +0200

    resolved

--- private/my-local-nsp-branch-config.el
+++ private/my-local-nsp-branch-config.el
@@ -1,5 +1,7 @@
-(nsp-add-branch "develop" "/home/johanst/extflash/develop/server")
-(nsp-add-branch "dsn-develop" "/home/johanst/dsn-develop/server")
-(nsp-add-branch "RB-1.5" "/home/johanst/exthd/RB-1.5/server")
+(nsp-add-branch "INT-V160" "/home/johanst/INT-V160/server")
+(nsp-add-branch "pb-develop" "/home/johanst/pb-develop/server")
+(nsp-add-branch "DEV-LON-IN-PCS" "/home/johanst/DEV-LON-IN-PCS/server")
+(nsp-add-branch "INT-V150" "/home/johanst/INT-V150/server")
+(nsp-add-branch "RB-1.5-AS" "/home/johanst/RB-1.5-AS/server")
 
 (provide 'my-local-nsp-branch-config)

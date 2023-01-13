8ae43c8824341b217e19ba4ad44066e35a06f699
Johan Sternerup <johast@axis.com>
2023-01-13 16:44:04 +0100

    Rustic mode in-place
    Change-Id: Ieb8b864c055e27d1a7de5ed5987a0aaf03b6f15a

--- init.el
+++ init.el
@@ -177,7 +177,7 @@
  ;; If there is more than one, they won't work right.
  '(org-todo-keywords '((sequence "TODO" "WAITING" "DONE")))
  '(package-selected-packages
-   '(lsp-mode flycheck-irony company-irony irony-eldoc irony helm-rtags company-rtags flycheck-rtags helm flycheck company rtags rust-mode go-mode meson-mode typescript-mode markdown-mode json-mode adoc-mode plantuml-mode zenburn-theme pyvenv magit key-chord clang-format ace-jump-mode))
+   '(dash lsp-mode flycheck-irony company-irony irony-eldoc irony helm-rtags company-rtags flycheck-rtags helm flycheck company rtags rust-mode go-mode meson-mode typescript-mode markdown-mode json-mode adoc-mode plantuml-mode zenburn-theme pyvenv magit key-chord clang-format ace-jump-mode))
  '(show-trailing-whitespace t)
  '(sql-sqlite-program "sqlite3")
  '(warning-suppress-types '(((package reinitialization))))
